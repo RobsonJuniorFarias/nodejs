f51559cbb0cdbe2c82155ba8bc1fe6bc34abeb25
RobsonJuniorFarias <fariasjuniorrobson@gmail.com>
2024-11-12 15:59:20 -0300

    if

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http'
 
 const server = http.createServer ((req, res) => {
+    const {method, url} = req
+
+    if (method = 'POST' && url === '/users')
+        return res.end ('Listagem de usuários')
+
+    if (method = 'GET' && url === '/users')
+    return res.end ('criaçãp de usuários')
+
+
+
     return res.end ('Hello Ignite')
 })
 
