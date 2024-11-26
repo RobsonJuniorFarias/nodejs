27afe2761ab9fbc3f28f684f13ffcdd25d7dc99e
RobsonJuniorFarias <fariasjuniorrobson@gmail.com>
2024-11-26 20:11:55 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -28,8 +28,22 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer ((req, res) => {
+const server = http.createServer (async(req, res) => {
     const { method, url } = req
+
+    const buffers = []
+
+    for await (const chunk of req){
+      buffers.push(chunk)
+    }
+  
+try{
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+} catch{
+    req.body = null
+}
+
+    console.log(req.body)
 
 if (method === 'GET' && url === '/users') {
     return res
@@ -38,10 +52,12 @@ if (method === 'GET' && url === '/users') {
     }
         
         if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+
         users.push({
             id: 1,
-            name: 'Robert Junior',
-            email: 'robertjunior@example.com',
+            name,
+            email,
         })
         
         return res.writeHead(201).end();
