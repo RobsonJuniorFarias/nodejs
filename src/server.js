5a7d63834e5406840fc8df4d5bb4d27cb4b0a968
RobsonJuniorFarias <fariasjuniorrobson@gmail.com>
2024-11-26 17:06:33 -0300

    CONSEGUI PEGAR A PRÁTICA

--- src/server.js
+++ src/server.js
@@ -24,26 +24,30 @@ import http from 'node:http'
 
 // Cabeçalhos (Requisição/resposta) => Metadados
 
+
+
 const users = []
 
 const server = http.createServer ((req, res) => {
-    const {method, url} = req
+    const { method, url } = req
 
-    if (method === 'POST' && url === '/users') {
+if (method === 'GET' && url === '/users') {
+    return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+        
+        if (method === 'POST' && url === '/users') {
         users.push({
             id: 1,
             name: 'Robert Junior',
             email: 'robertjunior@example.com',
         })
-            }
+        
         return res.writeHead(201).end();
-        
-        
-        if (method === 'GET' && url === '/users')
-    return res.setHeader('Content-Type', 'application/json');
-           res.end (JSON.strignify(users))
-    
-    return res.writehead(401).end ()
+        }
+
+        return res.writehead(401).end ()
 })
 
 server.listen(3333)
