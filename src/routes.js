1381fd78e2a75a760be5b48876b6f397bab66eef
RobsonJuniorFarias <fariasjuniorrobson@gmail.com>
2024-12-10 10:57:10 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,39 +1,40 @@
-import { Database } from "./database.js"
-import { randomUUID } from 'node:crypto'
+import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
 
-const database = new Database()
+const database = new Database();
 
 export const routes = [
-    {
-        method: 'GET',
-        path: '/users',
-        handler: (req, res) => {
-            const users = database.select('users')
+  {
+    method: "GET",
+    path: "/users",
+    handler: (req, res) => {
+      const users = database.select("users");
 
-            return res.end(JSON.stringify(users))  
-        }
+      return res.end(JSON.stringify(users));
     },
-    {
-        method: 'POST',
-        path: '/users',
-        handler: (req, res) => {
-            const { name, email } = req.body
+  },
+  {
+    method: "POST",
+    path: "/users",
+    handler: (req, res) => {
+      const { name, email } = req.body;
 
-            const user = {
-                    id: randomUUID(),
-                    name,
-                    email,
-                }
-        
-                database.insert('users', user)
-                
-                return res.writeHead(201).end();
-        }
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      };
+
+      database.insert("users", user);
+
+      return res.writeHead(201).end();
     },
-    {
-        method: 'DELETE',
-        path: '/users',
-        handler: (req, res) => {}
-            return res.end()
+  },
+  {
+    method: "DELETE",
+    path: "/users",
+    handler: (req, res) => {
+      return res.end();
     },
-]
+  },
+];
