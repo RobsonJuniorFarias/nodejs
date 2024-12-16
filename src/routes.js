471591defaa129dffa05b82dd022ace55960cf82
RobsonJuniorFarias <fariasjuniorrobson@gmail.com>
2024-12-16 16:06:44 -0300

    inicio do update

--- src/routes.js
+++ src/routes.js
@@ -43,4 +43,15 @@ export const routes = [
       return res.writeHead(204).end();
     },
   },
+  {
+  method: "PUT",
+    path: BuildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.update('users', id)
+
+      return res.writeHead(204).end();
+    }
+  },  
 ];
